c363a869df85a569f276d5e7c5ce25f7ac35432c
skyzaus <155588321+skyzaus@users.noreply.github.com>
2024-07-31 12:38:54 -0700

    udated css

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -2,7 +2,7 @@ import {Routes, Route, Navigate} from "react-router-dom";
 
 import Home from "../pages/Home/Home";
 import Spacecrafts from "../pages/Spacecrafts/Spacecrafts";
-// import SpacecraftBuild from "../pages/SpacecraftBuild/SpacecraftBuild";
+import SpacecraftBuild from "../pages/SpacecraftBuild/SpacecraftBuild";
 import Spacecraft from "../pages/Spacecraft/Spacecraft";
 import Planets from "../pages/Planets/Planets";
 
@@ -13,7 +13,7 @@ function AppRoute ()
         <Routes>
         <Route path="/"element={<Home />} />
        <Route path="/spacecrafts"element={<Spacecrafts />} />
-      {/* <Route path="/spacecraft/build" element={<SpacecraftBuild />}/> */}
+      <Route path="/spacecraft/build" element={<SpacecraftBuild />}/>
       <Route path="/spacecraft/:id" element={<Spacecraft />}/>
       <Route path="/planets" element={<Planets />} />
 <Route path="*" element={<Navigate to={"/"} />}/>
